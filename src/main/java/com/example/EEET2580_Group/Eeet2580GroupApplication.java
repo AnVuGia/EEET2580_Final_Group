4586eb6c7c3fe1b78bc13f1ca45addccc5f6a81e
An Vu <104729655+AnVuGia@users.noreply.github.com>
2023-03-30 11:02:21 +0700

    first commit

---
+++ src/main/java/com/example/EEET2580_Group/Eeet2580GroupApplication.java
@@ -0,0 +1,13 @@
+package com.example.EEET2580_Group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eeet2580GroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eeet2580GroupApplication.class, args);
+	}
+
+}
