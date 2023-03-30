054b67dc875e442b13526bfdb5ecc51ef30688cd
An Vu <104729655+AnVuGia@users.noreply.github.com>
2023-03-30 11:03:55 +0700

    Update Eeet2580GroupApplication.java

--- src/main/java/com/example/EEET2580_Group/Eeet2580GroupApplication.java
+++ src/main/java/com/example/EEET2580_Group/Eeet2580GroupApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Eeet2580GroupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Eeet2580GroupApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(Eeet2580GroupApplication.class, args);
+// 	}
 
 }
