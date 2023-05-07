84515cd5ee4f735a2960fb342c86b5446600872f
HyeonB7201 <130901366+HyeonB7201@users.noreply.github.com>
2023-05-07 22:26:13 +0700

    Edit student info by skills, persona, bib

--- src/main/java/com/example/EEET2580_Group/Controller/StudentController.java
+++ src/main/java/com/example/EEET2580_Group/Controller/StudentController.java
@@ -1,21 +1,39 @@
 package com.example.EEET2580_Group.Controller;
 
 
-
 import com.example.EEET2580_Group.DTO.StudentDto;
 import com.example.EEET2580_Group.Service.Interface.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/api")
 public class StudentController {
     @Autowired
     StudentService studentService;
-    @PutMapping(value = "/student/update/{id}")
-    public void updateStudentInfo(@PathVariable Long id,
+    @PutMapping(value = "/student/update/{id}/persona")
+    public void updateStudentPersonaInfo(@PathVariable Long id,
                                   @RequestBody StudentDto studentDto){
         System.out.println("update student");
-        studentService.updateStudentinfoById(id, studentDto);
+        studentService.updateStudentPersonaById(id, studentDto);
     }
+
+    @PutMapping(value = "/student/update/{id}/skills")
+    public void updateStudentSkillsInfo(@PathVariable Long id,
+                                  @RequestBody StudentDto studentDto){
+        System.out.println("update student");
+        studentService.updateStudentSkillsById(id, studentDto);
+    }
+
+    @PutMapping(value = "/student/update/{id}/Bib")
+    public void updateStudentBibInfo(@PathVariable Long id,
+                                     @RequestBody StudentDto studentDto){
+        System.out.println("update student");
+        studentService.updateStudentBibById(id, studentDto);
+    }
+
+
+
+
+
+
 }
